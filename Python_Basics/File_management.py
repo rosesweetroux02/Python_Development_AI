a4d712b6332488d8de27a85057056c4192c5e042
Rose Tebogo Ndhlovu <114318695+rosesweetroux02@users.noreply.github.com>
2026-02-02 09:10:18 +0200

    Updated

--- Python_Basics/File_management.py
+++ Python_Basics/File_management.py
@@ -2,7 +2,11 @@
 file = open("data.txt","w")
 file.write("Hello, this is a sample file.\nIt contains multiple lines of text.\nThis is the third line.")
 file.close()
+
+
 #  Opening a file and reading its content
 file =open("data.txt","r")
 content = file.read()
 print(content)
+file.close()
+
